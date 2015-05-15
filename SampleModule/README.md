0a99686abe92cbdd45dd03b1b2dfcb1bad782e05
kyrie_leison <yuka.sato@gaiax.com>
2015-05-15 12:58:19 +0900

    initial commit

---
+++ SampleModule/README.md
@@ -0,0 +1,22 @@
+# NAME
+
+SampleModule - It's new $module
+
+# SYNOPSIS
+
+    use SampleModule;
+
+# DESCRIPTION
+
+SampleModule is ...
+
+# LICENSE
+
+Copyright (C) kyrie\_leison.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kyrie\_leison <yuka.sato@gaiax.com>
---
+++ SampleModule/lib/SampleModule.pm
@@ -0,0 +1,39 @@
+package SampleModule;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+SampleModule - It's new $module
+
+=head1 SYNOPSIS
+
+    use SampleModule;
+
+=head1 DESCRIPTION
+
+SampleModule is ...
+
+=head1 LICENSE
+
+Copyright (C) kyrie_leison.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kyrie_leison E<lt>yuka.sato@gaiax.comE<gt>
+
+=cut
+
